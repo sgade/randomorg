8034772de4245b941e3773124aa533eb3203f1b2
Sören Gade <gade.soeren@gmail.com>
2015-09-25 15:15:42 +0200

    Added examples.

---
+++ example_test.go
@@ -0,0 +1,24 @@
+package randomorg_test
+
+import (
+	"fmt"
+	"github.com/sgade/randomorg"
+)
+
+const (
+	apiKey = "YOUR API KEY"
+)
+
+func Example() {
+	// create a new client
+	_ = randomorg.NewRandom(apiKey)
+	// call methods on the return value here
+}
+
+// Generate one int64 value from 0 to 10.
+func ExampleRandom_GenerateIntegers() {
+	random := randomorg.NewRandom(apiKey)
+	// generates a random value
+	value, _ := random.GenerateIntegers(1, 0, 10)
+	fmt.Printf("Random value: %v\n", value)
+}
